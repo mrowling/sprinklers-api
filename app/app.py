59a05213cc0a1b3350a500b3a81ebd0fc29daed3
Mitchell Rowling <mitchell.rowling@gmail.com>
2020-06-20 00:13:00 +1000

    Add CORS

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 import falcon
+from falcon_cors import CORS
+
 from .config import CONFIG
 from .sprinkler import SprinklerResource, SprinklerCollection
 
 
-api = application = falcon.API()
+cors = CORS(allow_origins_list=['*'])
+api = application = falcon.API(middleware=[cors.middleware])
 
 sprinklers = SprinklerResource(SprinklerCollection(CONFIG.get("sprinklers")))
 api.add_route("/sprinkler/{name}", sprinklers)
