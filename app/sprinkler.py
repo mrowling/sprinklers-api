f7e622fc400c9e82fc1cf5a33bc3ef0f97dd7a58
mrowlingfox <mitchell.rowling@foxsports.com.au>
2020-06-19 22:31:08 +1000

    Control Relay from another process

--- app/sprinkler.py
+++ app/sprinkler.py
@@ -1,3 +1,5 @@
+from multiprocessing import Process, Value
+
 from time import sleep
 from .relay import Relay
 
@@ -5,19 +7,38 @@ class Sprinkler(object):
     def __init__(self, channel):
         self.relay = Relay(channel)
         self._triggering = False
-        self._sleep_duration = 1000
+        self._sleep_duration = 1
+    
+    @property
+    def _triggering(self):
+        with self._triggering_value.get_lock():
+            return bool(self._triggering_value.value)
+    
+    @_triggering.setter
+    def _triggering(self, val):
+        try:
+            with self._triggering_value.get_lock():
+                self._triggering_value.value = val
+        except AttributeError:
+            self._triggering_value = Value("i", val)
+
 
     def _trigger(self):
-        self.relay.on()
-        sleep(self._sleep_duration)
-        self.relay.off()
-            
+        try:
+            self._triggering = True
+            self.relay.on()
+            sleep(self._sleep_duration)
+            self.relay.off()
+        finally:
+            self._triggering = False
     
     def trigger(self):
         if not self._triggering:
-            self._triggering = True
-            self._trigger()
-            self._triggering = False
+            process = Process(
+                target=self._trigger,
+                daemon=True
+            )
+            process.start()
         else:
             raise Exception("Already triggering")
     
