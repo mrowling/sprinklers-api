0698bcd7b75f8435a63229a275a752657deb503e
Mitchell Rowling <mitchell.rowling@gmail.com>
2020-06-20 00:21:21 +1000

    Move config to YAML

--- app/config.py
+++ app/config.py
@@ -1,35 +1,10 @@
-sprinklers = [{
-    "name": "NorthWest",
-    "short_name": "nw",
-    "output_channel": 1,
-    "input_channel": 1
-}, {
-    "name": "NorthEast",
-    "short_name": "ne",
-    "output_channel": 2,
-    "input_channel": 2
-}, {
-    "name": "MiddleWest",
-    "short_name": "mw",
-    "output_channel": 2,
-    "input_channel": 2
-}, {
-    "name": "MiddleEast",
-    "short_name": "MEme",
-    "output_channel": 2,
-    "input_channel": 2
-}, {
-    "name": "SouthWest",
-    "short_name": "sw",
-    "output_channel": 2,
-    "input_channel": 2
-}, {
-    "name": "SouthEast",
-    "short_name": "sw",
-    "output_channel": 2,
-    "input_channel": 2
-}]
+import yaml
 
-CONFIG = {
-    "sprinklers": sprinklers
-}
+
+def get_config():
+    with open(r'config.yaml') as file:
+        config = yaml.full_load(file)
+        return config
+
+
+CONFIG = get_config()
