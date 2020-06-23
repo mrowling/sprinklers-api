5b1ce01f75bd9486cb0e81369f378c3a6b8681b3
Mitchell Rowling <mitchell.rowling@gmail.com>
2020-06-24 03:41:37 +1000

    Add Power Resource

--- app/app.py
+++ app/app.py
@@ -7,6 +7,8 @@ from .gpio import gpio
 from .auth import auth_middleware
 from .sprinkler import SprinklerResource, SprinklerCollection
 from .pump import PumpResource, Pump
+from .relay import Relay
+from .power import PowerResource
 
 
 cors = CORS(
@@ -27,6 +29,10 @@ api.add_route("/pump", pump_resource)
 sprinkler = SprinklerCollection(CONFIG.get("sprinklers"), pump)
 sprinkler_resource = SprinklerResource(sprinkler)
 api.add_route("/sprinkler/{name}", sprinkler_resource)
+
+power = Relay(CONFIG.get("power").get("channel"))
+power_resource = PowerResource(power)
+api.add_route("/power", power_resource)
 
 
 @atexit.register
---
+++ app/power.py
@@ -0,0 +1,36 @@
+import json
+import falcon
+
+from .relay import Relay
+
+class PowerResource():  # pylint: disable=too-few-public-methods
+    def __init__(self, relay: Relay):
+        self.relay = relay
+
+    def on_get(self, req, resp):  # pylint: disable=unused-argument
+        doc = {
+            "on": bool(self.relay.state)
+        }
+        resp.body = json.dumps(doc, ensure_ascii=False)
+        resp.status = falcon.HTTP_200
+
+    def on_put(self, req, resp):  # pylint: disable=unused-argument
+        try:
+            if req.media.get("on"): 
+                self.relay.on()
+            else:
+                self.relay.off()
+            doc = {
+                "success": True,
+                "on": bool(self.relay.state)
+            }
+            resp.body = json.dumps(doc, ensure_ascii=False)
+            resp.status = falcon.HTTP_200
+        except Exception as error:  # pylint: disable=broad-except
+            doc = {
+                "success": False,
+                "message": str(error),
+                "on": bool(self.relay.state)
+            }
+            resp.body = json.dumps(doc, ensure_ascii=False)
+            resp.status = falcon.HTTP_200
