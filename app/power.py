1f9f620b43856829a1ad44db333b2e159a588358
Mitchell Rowling <mitchell.rowling@gmail.com>
2020-07-11 17:00:33 +1000

    Power button is now pulsed

--- app/power.py
+++ app/power.py
@@ -1,15 +1,16 @@
 import json
 import falcon
+from time import sleep
 
 from .relay import Relay
 from .input import Input
 from .pump import Pump
 
 
-class Power():
-    def __init__(self, _input: Input, output: Relay):
+class Power(Input):
+    def __init__(self, input_channel, output: Relay):
         self.output = output
-        self.input = _input
+        super().__init__(input_channel)
 
     def on(self):
         return self.output.on()
@@ -17,38 +18,32 @@ class Power():
     def off(self):
         return self.output.off()
 
-    def state(self):
-        return self.input.state
+    def pulse(self):
+        self.output.on()
+        sleep(1)
+        self.output.off()
 
 
 class PowerResource():  # pylint: disable=too-few-public-methods
     def __init__(self, power: Power, pump: Pump):
         self.power = power
         self.pump = pump
-        self.is_power_active = True
 
     def on_get(self, req, resp):  # pylint: disable=unused-argument
         doc = {
-            # "active": bool(self.power.state)
-            "active": self.is_power_active
+            "active": bool(self.power.state)
         }
         resp.body = json.dumps(doc, ensure_ascii=False)
         resp.status = falcon.HTTP_200
 
     def on_put(self, req, resp):  # pylint: disable=unused-argument
         try:
-            if req.media.get("active"):
-                self.power.on()
-                self.is_power_active = True
-            else:
-                self.power.off()
+            self.power.pulse()
+            if not req.media.get("active"):
                 self.pump.clear_running()
-                self.is_power_active = False
             doc = {
                 "success": True,
-                # "active": bool(self.power.state),
-                # "active": bool(req.media.get("active"))
-                "active": self.is_power_active
+                "active": bool(self.power.state)
             }
             resp.body = json.dumps(doc, ensure_ascii=False)
             resp.status = falcon.HTTP_200
