19b9f62b8f4a581945a8901ea940fcc7f6ddec8e
Mitchell Rowling <mitchell.rowling@gmail.com>
2020-07-11 17:18:25 +1000

    Sync to right dir

--- copy-to-pi.sh
+++ copy-to-pi.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rsync -azP --exclude '.git' --exclude-from='.gitignore' "$(pwd)" pi@10.0.0.15:/home/pi/Code/sdhc-sprinklers-api
+rsync -azP --exclude '.git' --exclude-from='.gitignore' "$(pwd)" pi@10.0.0.15:/home/pi/Code
 rsync -azP "$(pwd)/config.yaml"  pi@10.0.0.15:/home/pi/Code/sdhc-sprinklers-api/config.yaml
