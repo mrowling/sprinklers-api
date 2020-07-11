8564731764900f0a4ab4c2c23e2ee4951152c163
Mitchell Rowling <mitchell.rowling@gmail.com>
2020-07-11 19:27:58 +1000

    sync improvements

--- copy-to-pi.sh
+++ copy-to-pi.sh
@@ -2,7 +2,12 @@
 SRC="$(pwd)"
 DST="pi@10.0.0.15:/home/pi/Code"
 
-rsync -ah \
+rsync -ahzP \
+    --exclude '.git' \
     --exclude-from="$(git -C $SRC ls-files --exclude-standard -oi --directory > .git/ignores.tmp && echo .git/ignores.tmp)" \
     $SRC \
     $DST 
+
+rsync -ahzP \
+    $SRC/config.yaml \
+    $DST/sdhc-sprinklers-api/config.yaml
