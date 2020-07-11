36e84863233db182a4dd151925115a1f56d5c986
Mitchell Rowling <mitchell.rowling@gmail.com>
2020-07-12 00:20:19 +1000

    In Memory Lock on Relay

--- app/relay.py
+++ app/relay.py
@@ -8,9 +8,10 @@ bus = SMBus(DEVICE_BUS)
 
 
 class Relay():
-    def __init__(self, device, channel):
+    def __init__(self, device, channel, lock=None):
         self.device = device
         self.channel = channel
+        self._lock = lock
 
     def on(self):
         self._set_state(ON)
@@ -20,3 +21,14 @@ class Relay():
 
     def _set_state(self, state):
         bus.write_byte_data(self.device, self.channel, state)
+
+    @property
+    def lock(self):
+        if self._lock is not None:
+            return self._lock
+        return False
+
+    @lock.setter
+    def lock(self, value):
+        if self._lock is not None:
+            self._lock = value
